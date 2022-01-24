0aeeb4e799fc9b7e5160f47f1074e75abfd5736a
Anshul Khope <anshul@iocare.in>
2022-01-24 14:49:06 +0530

    Created `feedback` page

---
+++ src/app/pages/feedback/feedback.page.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-feedback',
+  templateUrl: './feedback.page.html',
+  styleUrls: ['./feedback.page.scss'],
+})
+export class FeedbackPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  submitFeedback() {
+    alert('Submitted.');
+  }
+
+}

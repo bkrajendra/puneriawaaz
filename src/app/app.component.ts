99659ec6da989437d6d2448fa3e2fb89b4dad086
Anshul Khope <anshul@iocare.in>
2022-01-24 14:50:02 +0530

    Added feedback page to menu routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,7 @@ export class AppComponent {
   public appPages = [
     {title: 'Radio', url: '/radio', icon: 'radio'},
     {title: 'Schedule', url: '/schedule', icon: 'calendar'},
-    // {title: 'Feedback', url: '/feedback', icon: 'share'},
+    {title: 'Feedback', url: '/feedback', icon: 'share'},
     {title: 'About', url: '/about', icon: 'help-circle'},
     {title: 'Contact', url: '/contact', icon: 'mail'},
     // {title: 'Join', url: '/join', icon: 'add-circle'},
